e38dfaa2bc8847bbd996cf90748e7a1680326976
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2022-01-06 22:15:57 -0800

    done for the night

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -1,3 +1,11 @@
-export default function Profile() {
-  return <h1>My Profile</h1>;
-}
+import React from 'react';
+import Header from '../../components/layout/Header';
+
+export const Layout = ({ children }) => {
+  return (
+    <div>
+      <Header />
+      <main>{children}</main>
+    </div>
+  );
+};
